8837992907564d497fa2d886238b6002bb42d972
Kris Warner <kdwarn@protonmail.com>
2020-08-04 17:47:13 -0400

    Render example bokeh component with django

--- main/views.py
+++ main/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from bokeh.plotting import figure
+from bokeh.embed import components
 
-# Create your views here.
+
+def index(request):
+    return render(request, "main/base.html")
+
+
+def test_bokeh(request):
+    # Graph X & Y coordinates
+    x = [1, 2, 3, 4, 5]
+    y = [1, 2, 3, 4, 5]
+
+    plot = figure(
+        title="Line Graph",
+        x_axis_label="X-axis",
+        y_axis_label="Y-axis",
+        plot_width=400,
+        plot_height=400,
+    )
+
+    plot.line(x, y, line_width=2)
+
+    script, div = components(plot)
+
+    return render(request, "main/base.html", {"script": script, "div": div})
