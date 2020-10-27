b9397bbacc100f27cfa83eae9c39b8c41afd538b
Kris Warner <kdwarn@protonmail.com>
2020-10-27 17:19:58 -0400

    Remove unnecessary files.

--- bokeh_apps/stationary_energy.py
+++
@@ -1,8 +0,0 @@
-from bokeh.layouts import column
-from bokeh.plotting import curdoc
-
-from ghg_calc import grid_inputs, res_inputs, ci_inputs, bar_chart, stacked_bar_chart, pie_chart
-
-
-curdoc().add_root(column(grid_inputs, res_inputs, ci_inputs))
-curdoc().add_root(column(bar_chart, stacked_bar_chart, pie_chart))
