f7b8d0be0e20d0d12a4d92a7da6ab4cd1c511b0b
Kris Warner <kdwarn@protonmail.com>
2021-09-13 11:18:49 -0400

    Use underscores in bokeh app files, update reqs

--- main/views.py
+++ main/views.py
@@ -19,50 +19,50 @@ def index(request):
 
 
 def pop(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh-pop", relative_urls=True)
+    script = server_document(url_prefix + "bokeh_pop")
     return render(request, "main/pop.html", dict(script=script))
 
 
 def electricity_grid(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh-grid", relative_urls=True)
+    script = server_document(url_prefix + "bokeh_grid", relative_urls=True)
     return render(request, "main/grid.html", dict(script=script))
 
 
 def res_stationary(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh-res", relative_urls=True)
+    script = server_document(url_prefix + "bokeh_res", relative_urls=True)
     return render(request, "main/res.html", dict(script=script))
 
 
 def non_res_stationary(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh-non-res", relative_urls=True)
+    script = server_document(url_prefix + "bokeh_non_res", relative_urls=True)
     return render(request, "main/non_res.html", dict(script=script))
 
 
 def on_road_motor_veh(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh-on-road", relative_urls=True)
+    script = server_document(url_prefix + "bokeh_on_road", relative_urls=True)
     return render(request, "main/on_road.html", dict(script=script))
 
 
 def rail(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh-rail", relative_urls=True)
+    script = server_document(url_prefix + "bokeh_rail", relative_urls=True)
     return render(request, "main/rail.html", dict(script=script))
 
 
 def aviation(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh-aviation", relative_urls=True)
+    script = server_document(url_prefix + "bokeh_aviation", relative_urls=True)
     return render(request, "main/aviation.html", dict(script=script))
 
 
 def mobile_other(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh-other", relative_urls=True)
+    script = server_document(url_prefix + "bokeh_other", relative_urls=True)
     return render(request, "main/other.html", dict(script=script))
 
 
 def non_energy(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh-non-energy", relative_urls=True)
+    script = server_document(url_prefix + "bokeh_non_energy", relative_urls=True)
     return render(request, "main/non_energy.html", dict(script=script))
 
 
 def sequestration_storage(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh-seq", relative_urls=True)
+    script = server_document(url_prefix + "bokeh_seq", relative_urls=True)
     return render(request, "main/seq.html", dict(script=script))
