f9e583aa316d6291f7f0a992978bf4dfceab3d15
Kris Warner <kdwarn@protonmail.com>
2021-11-05 16:15:21 -0400

    Enable development and production versions

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Greenhouse Gas Calculator
 
-This tool allows users to change various variables that contribute to greenhouse gas emissions and charts the results, comparing them to a baseline of the region's emissions in 2015.
+This tool allows users to change various inputs that contribute to greenhouse gas emissions and charts the results, comparing them to a baseline of the region's emissions in 2015.
+
+## Development Version
+
+Copy environment.py.example to environment.py and uncomment the variables under the development heading, then run the `dev_start` program. App will be available at http://localhost:8000.
 
 ## Tests
 
--- main/views.py
+++ main/views.py
@@ -1,17 +1,11 @@
-"""
-Bokeh apps are served up independently of this Django app, using the `bokeh serve` command. In
-production, that means that systemd service files are created for each bokeh app, and then nginx
-locations are used to proxy them to the appropriate path. Below, in the calls to
-`server_document`, that end of that nginx location url is used (with any path between it and
-the domain set in `url_prefix`.
-"""
-
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 from bokeh.embed import server_document
 
-url_prefix = "/app/ghg/"  # path after domain where the app lives
+from environment import bokeh_base_url, relative_urls
+
+url_prefix = "/app/ghg"
 
 
 def index(request):
@@ -19,50 +13,70 @@ def index(request):
 
 
 def pop(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh_pop")
+    script = server_document(
+        bokeh_base_url + url_prefix + "/bokeh_pop", relative_urls=relative_urls
+    )
     return render(request, "main/pop.html", dict(script=script))
 
 
 def electricity_grid(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh_grid", relative_urls=True)
+    script = server_document(
+        bokeh_base_url + url_prefix + "/bokeh_grid", relative_urls=relative_urls
+    )
     return render(request, "main/grid.html", dict(script=script))
 
 
 def res_stationary(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh_res", relative_urls=True)
+    script = server_document(
+        bokeh_base_url + url_prefix + "/bokeh_res", relative_urls=relative_urls
+    )
     return render(request, "main/res.html", dict(script=script))
 
 
 def non_res_stationary(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh_non_res", relative_urls=True)
+    script = server_document(
+        bokeh_base_url + url_prefix + "/bokeh_non_res", relative_urls=relative_urls
+    )
     return render(request, "main/non_res.html", dict(script=script))
 
 
 def on_road_motor_veh(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh_on_road", relative_urls=True)
+    script = server_document(
+        bokeh_base_url + url_prefix + "/bokeh_on_road", relative_urls=relative_urls
+    )
     return render(request, "main/on_road.html", dict(script=script))
 
 
 def rail(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh_rail", relative_urls=True)
+    script = server_document(
+        bokeh_base_url + url_prefix + "/bokeh_rail", relative_urls=relative_urls
+    )
     return render(request, "main/rail.html", dict(script=script))
 
 
 def aviation(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh_aviation", relative_urls=True)
+    script = server_document(
+        bokeh_base_url + url_prefix + "/bokeh_aviation", relative_urls=relative_urls
+    )
     return render(request, "main/aviation.html", dict(script=script))
 
 
 def mobile_other(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh_other", relative_urls=True)
+    script = server_document(
+        bokeh_base_url + url_prefix + "/bokeh_other", relative_urls=relative_urls
+    )
     return render(request, "main/other.html", dict(script=script))
 
 
 def non_energy(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh_non_energy", relative_urls=True)
+    script = server_document(
+        bokeh_base_url + url_prefix + "/bokeh_non_energy", relative_urls=relative_urls
+    )
     return render(request, "main/non_energy.html", dict(script=script))
 
 
 def sequestration_storage(request: HttpRequest) -> HttpResponse:
-    script = server_document(url_prefix + "bokeh_seq", relative_urls=True)
+    script = server_document(
+        bokeh_base_url + url_prefix + "/bokeh_seq", relative_urls=relative_urls
+    )
     return render(request, "main/seq.html", dict(script=script))
